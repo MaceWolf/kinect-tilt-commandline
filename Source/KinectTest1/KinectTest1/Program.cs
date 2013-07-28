998e6238998e487ffc8ad52e5a1ee2fa0d316ba0
haydster7 <haydster7@gmail.com>
2013-07-28 17:31:40 +1000

    Source code upload
    v1.0

---
+++ Source/KinectTest1/KinectTest1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace KinectTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 0;
+            /*Console.WriteLine("Number of command line parameters = {0}", args.Length);
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+                if (i == 0)
+                    val = Convert.ToInt32(args[i]);
+            }*/
+            val = Convert.ToInt32(args[0]);
+            
+
+            KinectSensor sensor = KinectSensor.KinectSensors[0];
+
+            sensor.Start();
+
+            sensor.ElevationAngle = Convert.ToInt32(val);
+
+            sensor.Stop();
+        }
+    }
+}
